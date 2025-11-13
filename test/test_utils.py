44f58d5c11800766b29bc3cfdf7daa9d85c1d2a6
Damon McCullough <damonmcc1391@gmail.com>
2025-11-13 13:55:18 -0500

    reformat tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,6 +5,7 @@ from utils.query import (
     explode_code_lists,
     explode_code,
     exclude_naics_codes,
+    exclude_naics_names,
     query_naics_codes,
 )
 
@@ -13,35 +14,25 @@ pd.set_option("display.max_columns", None)
 
 def test_find_permitted_naics_codes_minimum():
     district_uses = pd.DataFrame(
-        {
-            "Use NAICS Code": ["123", "456"],
-            "Zoning District": ["A1", "A1"],
-            "Not permitted": [False, True],
-        }
+        columns=["Use NAICS Code", "Zoning District", "Not permitted"],
+        data=[
+            ["123", "A1", False],
+            ["456", "A1", True],
+        ],
     )
+
     naics_codes = pd.DataFrame(
-        {
-            "NAICS Code": [
-                "123111",
-                "123112",
-                "123113",
-            ],
-            "Five-digit Group Code": [
-                "12311",
-                "12311",
-                "12311",
-            ],
-            "Four-digit Group Code": [
-                "1231",
-                "1231",
-                "1231",
-            ],
-            "Three-digit Group Code": [
-                "123",
-                "123",
-                "123",
-            ],
-        }
+        columns=[
+            "NAICS Code",
+            "Five-digit Group Code",
+            "Four-digit Group Code",
+            "Three-digit Group Code",
+        ],
+        data=[
+            ["123111", "12311", "1231", "123"],
+            ["123112", "12311", "1231", "123"],
+            ["123113", "12311", "1231", "123"],
+        ],
     )
     actual = find_permitted_naics_codes(district_uses, naics_codes)
     assert len(actual) == 3
@@ -49,18 +40,22 @@ def test_find_permitted_naics_codes_minimum():
 
 def test_explode_code_lists():
     input = pd.DataFrame(
-        {
-            "id": ["123", "1231"],
-            "listy": ["a, b, c", "d"],
-        }
+        columns=["id", "listy"],
+        data=[
+            ["123", "a, b, c"],
+            ["1231", "d"],
+        ],
     )
     # actual = explode_code_lists(input, "listy")
     actual = input.pipe(explode_code_lists, "listy")
     expected = pd.DataFrame(
-        {
-            "id": ["123", "123", "123", "1231"],
-            "listy": ["a", "b", "c", "d"],
-        }
+        columns=["id", "listy"],
+        data=[
+            ["123", "a"],
+            ["123", "b"],
+            ["123", "c"],
+            ["1231", "d"],
+        ],
     )
     pd.testing.assert_frame_equal(actual, expected)
 
@@ -82,63 +77,58 @@ def test_explode_code():
 
 def test_exclude_naics_codes():
     permitted_use_codes = pd.DataFrame(
-        {
-            "NAICS Code": ["123111", "123112", "123111", "456456", "456789", "456800"],
-            "Permitted value": ["123", "123", "1231", "456", "456", "456"],
-        }
+        columns=["Permitted value", "NAICS Code"],
+        data=[
+            ["123", "123111"],
+            ["123", "123112"],
+            ["1231", "123111"],
+            ["456", "456456"],
+            ["456", "456789"],
+            ["456", "456800"],
+        ],
     )
     district_uses = pd.DataFrame(
-        {
-            "Use NAICS Code": ["123", "1231", "456"],
-            "NAICS to subtract": ["123111", np.nan, "4567, 456800"],
-        }
+        columns=["Use NAICS Code", "NAICS to subtract"],
+        data=[
+            ["123", "123111"],
+            ["1231", np.nan],
+            ["456", "4567, 456800"],
+        ],
     )
     actual = permitted_use_codes.pipe(exclude_naics_codes, district_uses)
     expected = pd.DataFrame(
-        {
-            "NAICS Code": ["123112", "123111", "456456"],
-            "Permitted value": ["123", "1231", "456"],
-        }
+        columns=["Permitted value", "NAICS Code"],
+        data=[
+            ["123", "123112"],
+            ["1231", "123111"],
+            ["456", "456456"],
+        ],
     )
     pd.testing.assert_frame_equal(actual, expected)
 
 
 def test_query_naics_codes():
     district_uses = pd.DataFrame(
-        {
-            "Use NAICS Code": ["123", "456"],
-            "NAICS to subtract": ["12312, 123141", "456"],
-            "Zoning District": ["A1", "A1"],
-            "Not permitted": [False, True],
-        }
+        columns=["Use NAICS Code", "NAICS to subtract", "Zoning District", "Not permitted"],
+        data=[
+            ["123", "12312, 123141", "A1", False],
+            ["456", "456", "A1", True],
+        ],
     )
+
     naics_codes = pd.DataFrame(
-        {
-            "NAICS Code": [
-                "123111",
-                "123121",
-                "123131",
-                "123141",
-            ],
-            "Five-digit Group Code": [
-                "12311",
-                "12312",
-                "12313",
-                "12314",
-            ],
-            "Four-digit Group Code": [
-                "1231",
-                "1231",
-                "1231",
-                "1231",
-            ],
-            "Three-digit Group Code": [
-                "123",
-                "123",
-                "123",
-                "123",
-            ],
-        }
+        columns=[
+            "NAICS Code",
+            "Five-digit Group Code",
+            "Four-digit Group Code",
+            "Three-digit Group Code",
+        ],
+        data=[
+            ["123111", "12311", "1231", "123"],
+            ["123121", "12312", "1231", "123"],
+            ["123131", "12313", "1231", "123"],
+            ["123141", "12314", "1231", "123"],
+        ],
     )
     actual = query_naics_codes(
         district_uses,
@@ -148,7 +138,7 @@ def test_query_naics_codes():
     assert actual["NAICS Code"].to_list() == ["123111", "123131"]
 
 
-def test_query_naics_codes_mock(mock_uses_by_zoning_district, mock_naics_codes):
+def test_query_naics_codes_mock(mock_uses_by_zoning_district: pd.DataFrame, mock_naics_codes: pd.DataFrame):
     actual = query_naics_codes(
         mock_uses_by_zoning_district,
         "M2",
